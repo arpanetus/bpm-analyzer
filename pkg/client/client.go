f62f1cda48da7e0a535b13ba681bb051102b357b
Anvar Umarov <arpanetus@protonmail.com>
2021-12-03 02:34:04 +0600

    feat(client-lastfmer): create interface for a client

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -1,11 +1,18 @@
 package client
 
-
+import (
+	"context"
+	"github.com/arpanetus/bpm-analyzer/pkg/client/lastfm"
+)
 
 type LastFmer interface {
-
+	GetUserInfo(ctx context.Context, username string) (*lastfm.UserInfo, error)
+	GetRecentTracks(
+		ctx context.Context,
+		username string,
+		page, limit uint64,
+	) (*lastfm.RecentTracks, error)
 }
 
 type Spotifier interface {
-
 }
